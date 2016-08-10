9ecc4cf9ce04b731b283610723c81f3c3d1517ad
Quyen Tran <qtrandev@gmail.com>
2016-08-10 00:17:13 -0400

    Separate app into tabs

--- index.ios.js
+++ index.ios.js
@@ -14,14 +14,17 @@ import {
   Image,
   Text,
   TouchableHighlight,
-  View
+  View,
+  TabBarIOS,
+  ListView
 } from 'react-native';
 
 class miamitrolley extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      markers: props.markers
+      markers: props.markers,
+      selectedTab: 'one'
     };
   }
   componentDidMount() {
@@ -34,23 +37,60 @@ class miamitrolley extends Component {
   render() {
     return (
       <View style={styles.container}>
-          <MapView
-          style={styles.map}
-          region={{
-            latitude: 25.8011413,
-            longitude: -80.2044014,
-            latitudeDelta: 0.18,
-            longitudeDelta: 0.18
-          }}
-          annotations={this.state.markers}/>
-          <TouchableHighlight
-            style={styles.button}
-            onPress={() => this.refresh()}
-            underlayColor='#bbbbbb'>
-              <Text style={styles.buttonText}>
-                Refresh
-              </Text>
-          </TouchableHighlight>
+      <TabBarIOS
+        selectedTab={this.state.selectedTab}>
+        <TabBarIOS.Item
+        title="Routes"
+        systemIcon='bookmarks'
+        selected={this.state.selectedTab === 'zero'}
+        onPress={() => {
+          this.setState({
+            selectedTab: 'zero',
+          });
+        }}>
+          <RoutesView />
+        </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title="Map"
+            systemIcon='more'
+            selected={this.state.selectedTab === 'one'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'one',
+              });
+            }}>
+            <View style={styles.tabcontainer}>
+              <MapView
+              style={styles.map}
+              region={{
+                latitude: 25.8011413,
+                longitude: -80.2044014,
+                latitudeDelta: 0.18,
+                longitudeDelta: 0.18
+              }}
+              annotations={this.state.markers}/>
+              <TouchableHighlight
+                style={styles.button}
+                onPress={() => this.refresh()}
+                underlayColor='#bbbbbb'>
+                  <Text style={styles.buttonText}>
+                    Refresh
+                  </Text>
+              </TouchableHighlight>
+            </View>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+          title="Favorites"
+          systemIcon='favorites'
+          selected={this.state.selectedTab === 'two'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'two',
+            });
+          }}>
+            <FavoritesView />
+          </TabBarIOS.Item>
+        </TabBarIOS>
       </View>
     );
   }
@@ -84,6 +124,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  tabcontainer: {
+    flex: 1,
+    marginBottom: 50
+  },
   map: {
     flex: 1
   },
@@ -100,10 +144,71 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: '#fff'
   },
+  rowText1: {
+    padding: 10,
+    fontSize: 36,
+    color: 'blue'
+  },
+  rowText2: {
+    padding: 10,
+    fontSize: 24,
+    color: 'green'
+  }
 });
 
 miamitrolley.defaultProps = {
   markers: []
 };
+
+class RoutesView extends Component {
+  constructor(props) {
+    super(props);
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'Allapattah',
+        'Biscayne',
+        'Coral Way',
+        'Health District',
+        'Overtown',
+        'Stadium',
+        'Coconut Grove',
+        'Little Havana',
+        'Wynwood'
+      ])
+    };
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text style={styles.rowText1}>{rowData}</Text>}
+      />
+    );
+  }
+}
+
+class FavoritesView extends Component {
+  constructor(props) {
+    super(props);
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'Allapattah',
+        'Biscayne'
+      ])
+    };
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text style={styles.rowText2}>{rowData}</Text>}
+      />
+    );
+  }
+}
 
 AppRegistry.registerComponent('miamitrolley', () => miamitrolley);
